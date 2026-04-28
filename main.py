91e90015ba078ef41ac7e936a17bab7b3c01ea05
George Soulantikas <soulgeo01@gmail.com>
2026-04-28 12:11:31 +0300

    generate random genes and create datasets

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
+import random
+from collections import Counter
+
 from src.generate import generate_string
 
+A_COUNT = 20
+B_COUNT = 140
+C_COUNT = 40
+GENE_COUNT = A_COUNT + B_COUNT + C_COUNT
+
+
+def remove_sampled_elements(original_list, sampled_list):
+    counts = Counter(original_list)
+    to_remove = Counter(sampled_list)
+
+    remaining_counts = counts - to_remove
+
+    return list(remaining_counts.elements())
+
+
 def main():
-    gene = generate_string()
-    print(gene)
+    genes = []
+    for _ in range(GENE_COUNT):
+        genes += [generate_string()]
+
+    dataset_a = random.sample(genes, A_COUNT)
+    genes = remove_sampled_elements(genes, dataset_a)
+
+    dataset_b = random.sample(genes, B_COUNT)
+    genes = remove_sampled_elements(genes, dataset_b)
+
+    dataset_c = random.sample(genes, C_COUNT)
+
+    print(f"Dataset A: {dataset_a}")
+    print(f"Dataset B: {dataset_b}")
+    print(f"Dataset C: {dataset_c}")
+
 
 if __name__ == "__main__":
     main()
--- src/generate.py
+++ src/generate.py
@@ -1,7 +1,7 @@
 from random import choice, randint
 
-alphabet = {"A", "C", "G", "T"}
-patterns = ["ΑGAΤΤΑ", "ΑCTTTGCΑ", "ΑACTGGCAA", "CATTCAGT"]
+ALPHABET = {"A", "C", "G", "T"}
+PATTERNS = ["ΑGAΤΤΑ", "ΑCTTTGCΑ", "ΑACTGGCAA", "CATTCAGT"]
 
 a_min, a_max = 1, 3
 b_min, b_max = 1, 2
@@ -9,26 +9,26 @@ c_min, c_max = 1, 2
 
 
 def generate_string():
-    out = ""
+    output = ""
 
     a_count = randint(a_min, a_max)
     for _ in range(a_count):
-        out += choice(list(alphabet))
+        output += choice(list(ALPHABET))
 
-    for pattern in patterns:
+    for pattern in PATTERNS:
         mutated = pattern
         b_count = randint(b_min, b_max)
         for _ in range(b_count):
             index = randint(0, len(mutated) - 1)
             mutated = (
                 mutated[:index]
-                + choice(list(alphabet | {""}))
+                + choice(list(ALPHABET | {""}))
                 + mutated[index + 1 :]
             )
-        out += mutated
+        output += mutated
 
     c_count = randint(c_min, c_max)
     for _ in range(c_count):
-        out += choice(list(alphabet))
+        output += choice(list(ALPHABET))
 
-    return out
+    return output
