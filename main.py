6769cd5f5a710bc3f75216f1caee07fec2ec0faf
George Soulantikas <soulgeo01@gmail.com>
2026-04-28 11:42:28 +0300

    run uv init

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.generate import generate_string
+
+def main():
+    gene = generate_string()
+    print(gene)
+
+if __name__ == "__main__":
+    main()
--- src/main.py
+++
@@ -1,4 +0,0 @@
-from generate import generate_string
-
-gene = generate_string()
-print(gene)
