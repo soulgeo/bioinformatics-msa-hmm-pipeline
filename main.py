5c697633df441220bd6e0c8be17afef81fc109bc
George Soulantikas <soulgeo01@gmail.com>
2026-04-28 14:30:49 +0300

    save datasets to files

--- main.py
+++ main.py
@@ -1,39 +1,21 @@
-import random
-from collections import Counter
-
-from src.generate import generate_string
+from src.generate import generate_datasets
 
 A_COUNT = 20
 B_COUNT = 140
 C_COUNT = 40
-GENE_COUNT = A_COUNT + B_COUNT + C_COUNT
 
 
-def remove_sampled_elements(original_list, sampled_list):
-    counts = Counter(original_list)
-    to_remove = Counter(sampled_list)
-
-    remaining_counts = counts - to_remove
-
-    return list(remaining_counts.elements())
+def create_datasets():
+    datasets = generate_datasets(A_COUNT, B_COUNT, C_COUNT)
+    for i, dataset in enumerate(datasets):
+        letter = chr(ord('`') + i + 1)
+        with open(f"datasets/dataset_{letter}.txt", "w") as f:
+            output = "\n".join(dataset)
+            f.write(output)
 
 
 def main():
-    genes = []
-    for _ in range(GENE_COUNT):
-        genes += [generate_string()]
-
-    dataset_a = random.sample(genes, A_COUNT)
-    genes = remove_sampled_elements(genes, dataset_a)
-
-    dataset_b = random.sample(genes, B_COUNT)
-    genes = remove_sampled_elements(genes, dataset_b)
-
-    dataset_c = random.sample(genes, C_COUNT)
-
-    print(f"Dataset A: {dataset_a}")
-    print(f"Dataset B: {dataset_b}")
-    print(f"Dataset C: {dataset_c}")
+    create_datasets()
 
 
 if __name__ == "__main__":
--- src/generate.py
+++ src/generate.py
@@ -1,4 +1,5 @@
-from random import choice, randint
+from collections import Counter
+import random
 
 ALPHABET = {"A", "C", "G", "T"}
 PATTERNS = ["AGATTA", "ACTTTGCA", "AACTGGCAA", "CATTCAGT"]
@@ -8,27 +9,53 @@ B_MIN, B_MAX = 0, 2
 C_MIN, C_MAX = 1, 3
 
 
-def generate_string():
+def generate_gene():
     output = ""
 
-    a_count = randint(A_MIN, A_MAX)
+    # a) random symbols at the beginning
+    a_count = random.randint(A_MIN, A_MAX)
     for _ in range(a_count):
-        output += choice(list(ALPHABET))
+        output += random.choice(list(ALPHABET))
 
+    # b) process pattern with mutations
+    a_count = random.randint(A_MIN, A_MAX)
     for pattern in PATTERNS:
         mutated = pattern
-        b_count = randint(B_MIN, B_MAX)
+        b_count = random.randint(B_MIN, B_MAX)
         for _ in range(b_count):
-            index = randint(0, len(mutated) - 1)
+            index = random.randint(0, len(mutated) - 1)
             mutated = (
                 mutated[:index]
-                + choice(list(ALPHABET | {""}))
+                + random.choice(list(ALPHABET | {""}))
                 + mutated[index + 1 :]
             )
         output += mutated
 
-    c_count = randint(C_MIN, C_MAX)
+    # c) random symbols at the end
+    c_count = random.randint(C_MIN, C_MAX)
     for _ in range(c_count):
-        output += choice(list(ALPHABET))
+        output += random.choice(list(ALPHABET))
 
     return output
+
+
+def remove_sampled_genes(original, sampled):
+    counts = Counter(original)
+    to_remove = Counter(sampled)
+
+    remaining_counts = counts - to_remove
+
+    return list(remaining_counts.elements())
+
+
+def generate_datasets(*args: int):
+    genes = []
+    for _ in range(sum(args)):
+        genes += [generate_gene()]
+
+    datasets = []
+    for i, val in enumerate(args):
+        datasets += [random.sample(genes, val)]
+        genes = remove_sampled_genes(genes, datasets[i])
+
+    return datasets
