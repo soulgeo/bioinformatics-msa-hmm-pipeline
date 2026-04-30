4fc1f1144e120d7708a5de775ef63e46ef469f52
George Soulantikas <soulgeo01@gmail.com>
2026-04-30 18:39:30 +0300

    main

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import json
 import os
 
-from src.alignment import msa
-from src.generate import generate_datasets
-from src.hmm import (
+from src.training.alignment import msa
+from src.generation.generate import generate_datasets, generate_random_gene
+from src.training.hmm import (
     calculate_emissions_matrix,
     calculate_transitions_matrix,
     create_states_sequence,
 )
-from src.viterbi import viterbi
-from src.retrain import retrain_emissions_matrix, retrain_transitions_matrix
+from src.training.viterbi import forward_algorithm, viterbi 
+from src.training.retrain import retrain_emissions_matrix, retrain_transitions_matrix
 
 A_COUNT = 20
 B_COUNT = 140
@@ -39,48 +39,49 @@ def main():
     if not all(os.path.exists(f) for f in expected_files):
         create_datasets()
 
-    dataset_a = []
+    datasets = {}
+    for char in ['a', 'b', 'c']:
+        datasets[char] = []
+        file = open("datasets/dataset_a.txt", "r")
+        for line in file:
+            datasets[char].append(line.strip())
+        file.close()
 
-    file_a = open("datasets/dataset_a.txt", "r")
-    for line in file_a:
-        dataset_a.append(line.strip())
-    file_a.close()
-    
-    dataset_b = []
-
-    file_b = open("datasets/dataset_b.txt", "r")
-    for line in file_b:
-        dataset_b.append(line.strip())
-    file_b.close()
-
-    msa_a = msa(dataset_a)
+    msa_a = msa(datasets['a'])
 
     # 1. Build the Initial Draft HMM (from Dataset A)
     states_seq_a = create_states_sequence(msa_a)
-    num_matches = states_seq_a.count("M") # Save this architectural constant!
+    num_matches = states_seq_a.count("M")
     
     initial_emissions = calculate_emissions_matrix(msa_a, states_seq_a)
     initial_transitions = calculate_transitions_matrix(msa_a, states_seq_a)
 
-    # 2. Viterbi Training (Guesstimating the paths for Dataset B)
+    # 2. Viterbi Training
     dataset_b_paths = []
-    dataset_b_clean = []
     
-    for seq in dataset_b:
-        # Crucial: Remove all gaps from Dataset B before running Viterbi
-        clean_seq = seq.replace("-", "") 
-        dataset_b_clean.append(clean_seq)
-        
-        path = viterbi(clean_seq, initial_emissions, initial_transitions)
+    for gene in datasets['b']:
+        path = viterbi(gene, initial_emissions, initial_transitions)
         dataset_b_paths.append(path)
 
-    # 3. Generating the Trained HMM!
-    trained_emissions = retrain_emissions_matrix(dataset_b_clean, dataset_b_paths, num_matches)
+    # 3. Generating the Trained HMM
+    trained_emissions = retrain_emissions_matrix(datasets['b'], dataset_b_paths, num_matches)
     trained_transitions = retrain_transitions_matrix(dataset_b_paths, num_matches)
 
-    print("HMM Successfully Trained!")
-    print(json.dumps(trained_emissions, indent=2))
-    print(json.dumps(trained_transitions, indent=2))
+    dataset_c_scores = []
+    for gene in datasets['c']:
+        score = forward_algorithm(gene, trained_emissions, trained_transitions)
+        dataset_c_scores.append(score)
+
+    print(dataset_c_scores)
+
+    random_genes = [generate_random_gene() for _ in range(40)]
+    random_genes_scores = []
+    for gene in random_genes:
+        score = forward_algorithm(gene, trained_emissions, trained_transitions)
+        random_genes_scores.append(score)
+
+    print(random_genes_scores)
+
 
 if __name__ == "__main__":
     main()
